424f61155c5dadb1fbb377ca33ec418265b4f29b
apporc <appleorchard2000@gmail.com>
2022-10-11 15:45:59 +0800

    initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# Docker Images
+
+* [Antelope Leap](https://github.com/AntelopeIO/leap)
+* [EOS Chronicle](https://github.com/EOSChronicleProject/eos-chronicle)
+* [EOS LightAPI](https://github.com/cc32d9/eosio_light_api)
---
+++ lightapi/add_basic_tables.sql
@@ -0,0 +1,21 @@
+use lightapi;
+
+CREATE TABLE NETWORKS
+(
+ network           VARCHAR(15) PRIMARY KEY,
+ chainid           VARCHAR(64) NOT NULL,
+ description       VARCHAR(256) NOT NULL,
+ systoken          VARCHAR(7) NOT NULL,
+ decimals          TINYINT NOT NULL,
+ production        TINYINT NOT NULL DEFAULT 1,
+ rex_enabled       TINYINT NOT NULL DEFAULT 0
+) ENGINE=InnoDB;
+
+
+CREATE TABLE SYNC
+(
+ network           VARCHAR(15) PRIMARY KEY,
+ block_num         BIGINT NOT NULL,
+ block_time        DATETIME NOT NULL,
+ irreversible      BIGINT NOT NULL
+) ENGINE=InnoDB;
